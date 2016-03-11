1f3eb97f638477615623a1ea008c4c9200f46bef
himselfv@gmail.com <himselfv@gmail.com>
2016-03-11 14:29:24 +0300

    Added test script.

---
+++ examples/_runtests.cmd
@@ -0,0 +1,28 @@
+@echo off
+md Databases
+md Dumps
+call :process BasicTables.sql
+call :process Comments.sql
+call :process DataTypes.sql
+call :process AutoIncFields.sql
+call :process Indices.sql
+call :process PrivateComments.sql
+call :process PrivateAttrs.sql
+call :process Procedures.sql
+call :process Views.sql
+call :process Constraints.sql
+goto :eof
+
+:process
+echo %1 -- %~dp1Databases\%~n1.mdb
+..\jet.exe exec -f %~dp1Databases\%~n1.mdb -new -force -stdi %1
+if errorlevel 1 (
+  echo %1: execution failed.
+  goto :eof
+)
+..\jet.exe dump -f %~dp1Databases\%~n1.mdb -stdo %~dp1Dumps\%~nx1
+if errorlevel 1 (
+  echo %1: dump failed.
+  goto :eof
+)
+goto :eof
